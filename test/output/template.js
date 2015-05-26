be50908cb15f7f42a0b06b04551127250fe78410
Victor Perron <victor.perron@polyconseil.fr>
2015-05-26 16:29:16 +0200

    Improve template jshinting

--- test/output/template.js
+++ test/output/template.js
@@ -1,24 +1,20 @@
 angular.module('template.js', [])
-  .run(['$templateCache', function($templateCache) {
-    $templateCache.put('/foo/file1.tpl.jade',
-    '\n' +
+.run(['$templateCache', function($templateCache) {
+    $templateCache.put('/foo/file1.tpl.jade', '\n' +
     '<div>\n' +
     '  <h1>Hello World from file1!</h1>\n' +
-    '</div>')
-  $templateCache.put('/foo/file2.tpl.jade',
-    '\n' +
+    '</div>');
+  $templateCache.put('/foo/file2.tpl.jade', '\n' +
     '<div>\n' +
     '  <h1>Hello World from file2!</h1>\n' +
-    '</div>')
-  $templateCache.put('/foo/subfolder/subfile1.tpl.jade',
-    '\n' +
+    '</div>');
+  $templateCache.put('/foo/subfolder/subfile1.tpl.jade', '\n' +
     '<div>\n' +
     '  <h1>Hello World from subfile1!</h1>\n' +
-    '</div>')
-  $templateCache.put('/foo/subfolder/subfile2.tpl.jade',
-    '\n' +
+    '</div>');
+  $templateCache.put('/foo/subfolder/subfile2.tpl.jade', '\n' +
     '<div>\n' +
     '  <h1>Hello World from subfile2!</h1>\n' +
-    '</div>')
+    '</div>');
 
-  }]);
+}]);
